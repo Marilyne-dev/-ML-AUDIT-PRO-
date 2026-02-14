f37c2ad562bd24af3454a0361a5b16a6f14149ca
Marilyne-dev <marilyneaniambossou@gmail.com>
2026-02-14 16:08:00 +0100

    Fix React 19 dependency conflict with npmrc

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600, // Augmente la limite pour éviter les warnings
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Regroupe les dépendances dans un seul fichier
+          }
+        }
+      }
+    }
+  }
 })
